86c3368f6190d93c4a4eeb47def6c6d6c732aea9
Sabır Akbana <s.akbana9o@gmail.com>
2021-04-09 06:27:08 +0300

    10-2

---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userdal;
+
+        public UserManager(IUserDal userdal)
+        {
+            _userdal = userdal;
+        }
+
+        public IResult Add(User user)
+        {
+            _userdal.Add(user);
+            return new SuccessResult(Messages.UserAdded);
+        }
+
+        public IResult Delete(User user)
+        {
+            _userdal.Delete(user);
+            return new SuccessResult(Messages.UserDeleted);
+        }
+
+        public IDataResult<List<User>> GetAll()
+        {
+            return new SuccessDataResult<List<User>>(_userdal.GetAll(), Messages.UserListed);
+        }
+
+        public IDataResult<User> GetById(int userID)
+        {
+            return new SuccessDataResult<User>(_userdal.Get(u=>u.Id==userID));
+        }
+
+        public IResult Update(User user)
+        {
+            _userdal.Update(user);
+            return new SuccessResult(Messages.UserUpdated);
+        }
+    }
+}
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -10,5 +10,18 @@ namespace Business.Constants
         public static string CarNameInvalid = "Araç ismi en az 2 karakter olmalıdır";
         public static string CarPriceInvalid = "Araç fiyatı sıfırdan küçük olamaz";
         public static string CarListed = "Araçlar listelendi";
+        public static string UserAdded = "Kullanıcı eklendi";
+        public static string UserDeleted = "Kullanıcı silindi";
+        public static string UserUpdated = "Kullanıcı güncellendi";
+        public static string UserListed = "Kullanıcılar listelendi";
+        public static string CutomerAdded = "Müşteri eklendi";
+        public static string CutomerDeleted = "Müşteri silindi";
+        public static string CutomerUpdated = "Müşteri güncellendi";
+        public static string CutomerListed = "Müşteriler listelendi";
+        public static string RentalInvalid = "Şu an kiralanamaz";
+        public static string RentalSucces = "Araç kiralandı";
+        public static string RentalDeleted = "Silindi";
+        public static string RentalUpdated = "Güncellendi";
+        public static string RentalListed = "Kiralama listesi";
     }
 }
